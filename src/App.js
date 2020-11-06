17696960865b0fb6a18996ea6ce9a5234be18385
André Belhocine <dredtrake@gmail.com>
2020-11-06 15:53:23 +0100

    first commit, adding react-create-app as a starting point

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './img/tier-logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
