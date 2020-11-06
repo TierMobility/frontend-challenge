fd169c7f5f84a3eb765877cf9df217985c8b4c1c
vasilis kyritsis <vasilis@getpushme.com>
2020-11-06 19:15:53 +0200

    Change readme and more

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import logo from './img/tier-logo.svg';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Change mobility for good</p>
+        </div>
+    );
 }
 
 export default App;
