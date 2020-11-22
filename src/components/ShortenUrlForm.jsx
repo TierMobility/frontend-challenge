2ff24199b7825be3a0bd89b8c95552230255e384
Andrii Malykhin <andrii.malykhin@tech.better.team>
2020-11-22 12:54:16 +0100

    form with validation and status

--- src/components/ShortenUrlForm.jsx
+++ src/components/ShortenUrlForm.jsx
@@ -1,30 +1,46 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { createShortLink } from '../services/shortenService';
+import { validateURL } from '../helpers/url';
+import { statuses } from '../constants/statuses';
 
 const ShortenUrlForm = () => {
-    const [value, setValue] = useState('');
-
-    const onChange = useCallback((e) => {
-        setValue(e.target.value);
-    }, []);
+    const inputRef = useRef(null);
+    const [status, setStatus] = useState('');
 
     const onSubmit = useCallback((e) => {
         e.preventDefault();
 
-        createShortLink(value);
+        if (!inputRef.current) {
+            return;
+        }
+
+        const { value } = inputRef.current;
+
+        const isUrlValid = validateURL(value);
+
+        if (isUrlValid) {
+            setStatus('');
+            createShortLink(value);
+
+            // here is possible to set status from bit.ly response or our custom one
+        } else {
+            setStatus(statuses.invalidLink);
+        }
     }, []);
 
     return (
         <form onSubmit={onSubmit}>
             <label htmlFor="shorten">
                 Url:
-                <input placeholder="Url to shorten" id="shorten" type="text" value={value} onChange={onChange} />
+                <input
+                    ref={inputRef}
+                    placeholder="Url to shorten"
+                    id="shorten"
+                    type="text"
+                />
             </label>
             <input type="submit" value="Shorten and copy URL" />
-            {/* TODO: show below only when the url has been shortened and copied */}
-            <div>
-                {/* Show shortened url --- copied! */}
-            </div>
+            {status && <div>{status}</div>}
         </form>
     );
 };
